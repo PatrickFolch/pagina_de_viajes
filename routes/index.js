983d1c01cc734b3c03a4fdd4546ee1fa8cd771d7
Patrick Folch <patrickfolch2@gmail.com>
2018-06-05 20:58:19 +0200

    crear navbar responsive

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Viaja!' });
 });
 
-router.get('/quienessomos', function(req, res, next) {
-  res.render('quienessomos', { title: 'QUIENES SOMOS' });
-});
 
-router.get('/destinos', function(req, res, next) {
-  res.render('destinos', { title: 'DESTINOS' });
-});
 
-router.get('/dondeestamos', function(req, res, next) {
-  res.render('dondeestamos', { title: 'Donde Estamos' });
-});
 
 module.exports = router;
