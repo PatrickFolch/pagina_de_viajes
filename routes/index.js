4ead0cda49e14b02709e9d59ea65472951ef1c01
Patrick Folch <patrickfolch2@gmail.com>
2018-06-11 18:16:34 +0200

    fronted login terminado

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Viaja!' });
+  res.render('index', { title: 'Geekshubs travels' });
 });
 
-
+router.get('/login',function(req,res,next){
+  res.render('login', { title:'Login'});
+});
 
 
 module.exports = router;
